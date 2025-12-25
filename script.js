da049af909f61cdbe94e646f3194f03a897d73c6
sammar abbass <sammarabbass416@gmail.com>
2025-12-25 15:23:44 +0500

    protofilo commit

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
-  });
-});
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute('href'))
+//       .scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
 
+
+
+  document.getElementById("downloadBtn").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = "cv.pdf"; // path to your PDF
+    link.download = "SummerPlays_CV.pdf"; // name for download
+    link.click();
+  });
